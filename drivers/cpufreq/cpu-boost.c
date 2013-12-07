f8144014a76fc6c3e3ce34c14160abc8622a7f32
yank555-lu <yank555.lu@gmail.com>
2013-12-07 16:22:44 +0100

    cpufreq: update cpu-boost driver to make it behave with cpufreq hardlimit (Yank555.lu)

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -21,6 +21,9 @@
 #include <linux/jiffies.h>
 #include <linux/kthread.h>
 #include <linux/moduleparam.h>
+#ifdef CONFIG_CPUFREQ_HARDLIMIT
+#include <linux/cpufreq_hardlimit.h>
+#endif
 
 struct cpu_sync {
 	struct task_struct *thread;
@@ -63,8 +66,11 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val, voi
 		 cpu, policy->min);
 	pr_debug("CPU%u boost min: %u kHz\n", cpu, min);
 
+#ifdef CONFIG_CPUFREQ_HARDLIMIT
+	cpufreq_verify_within_limits(policy, min, check_cpufreq_hardlimit(policy->max)); /* Yank555.lu - Enforce hardlimit */
+#else
 	cpufreq_verify_within_limits(policy, min, UINT_MAX);
-
+#endif
 	pr_debug("CPU%u policy min after boost: %u kHz\n",
 		 cpu, policy->min);
 
@@ -120,17 +126,33 @@ static int boost_mig_sync_thread(void *data)
 			continue;
 		}
 
+#ifdef CONFIG_CPUFREQ_HARDLIMIT
+		if (sync_threshold && (dest_policy.cur >= check_cpufreq_hardlimit(sync_threshold))) /* Yank555.lu - Enforce hardlimit */
+#else
 		if (sync_threshold && (dest_policy.cur >= sync_threshold))
+#endif
 			continue;
 
 		cancel_delayed_work_sync(&s->boost_rem);
 		if (sync_threshold) {
 			if (src_policy.cur >= sync_threshold)
+#ifdef CONFIG_CPUFREQ_HARDLIMIT
+				s->boost_min = check_cpufreq_hardlimit(sync_threshold); /* Yank555.lu - Enforce hardlimit */
+#else
 				s->boost_min = sync_threshold;
+#endif
 			else
+#ifdef CONFIG_CPUFREQ_HARDLIMIT
+				s->boost_min = check_cpufreq_hardlimit(src_policy.cur); /* Yank555.lu - Enforce hardlimit */
+#else
 				s->boost_min = src_policy.cur;
+#endif
 		} else {
+#ifdef CONFIG_CPUFREQ_HARDLIMIT
+			s->boost_min = check_cpufreq_hardlimit(src_policy.cur); /* Yank555.lu - Enforce hardlimit */
+#else
 			s->boost_min = src_policy.cur;
+#endif
 		}
 		/* Force policy re-evaluation to trigger adjust notifier. */
 		cpufreq_update_policy(dest_cpu);
